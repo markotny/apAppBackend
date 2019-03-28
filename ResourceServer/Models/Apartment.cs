8e1c0d484191f6c0b68955553fef558c93377000
Unknown <bartekkg1997@gmail.com>
2019-03-28 03:04:34 +0100

    QuickFix

--- ResourceServer/Models/Apartment.cs
+++ ResourceServer/Models/Apartment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,13 @@ namespace ResourceServer.Models
 {
     public class Apartment
     {
-        public int ID_Apartment { get; set; }
+        public int ID_Ap { get; set; }
         public String Name { get; set; }
         public String City { get; set; }
         public String Street { get; set; }
         public String Address { get; set; }
         public String ImgThumb { get; set; }
+        public String[] ImgList { get; set; }
         public decimal? Rate { get; set; }
         public decimal Lat { get; set; }
         public decimal Long { get; set; }
