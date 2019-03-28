9f86e63f9b8e43f51252cdf7872d7e1346db9a51
Unknown <bartekkg1997@gmail.com>
2019-03-28 03:04:00 +0100

    Created REST API for Apartment
    Created methods to get whole model of Apartment from database and created json compatible controller

---
+++ ResourceServer/Controllers/ApartmentsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ResourceServer.Models;
+
+namespace ResourceServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class ApartmentsController : ControllerBase
+    {
+        // GET: api/Apartments
+        [HttpGet]
+        public string Get()
+        {
+            IEnumerable<Apartment> aps = TrueHomeContext.getAllApartments();
+            return JsonConvert.SerializeObject(aps, Formatting.Indented);
+        }
+
+        // GET: api/Apartments/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            Apartment ap = TrueHomeContext.getApartment(id);
+            return JsonConvert.SerializeObject(ap, Formatting.Indented);
+        }
+
+        // CREATE POST: api/Apartments
+        [HttpPost]
+        public async Task<IActionResult> Post(Apartment ap)
+        {
+            TrueHomeContext.createApartment(ap);
+            return NoContent();
+        }
+
+        // UPDATE PUT: api/Apartments/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Apartment ap)
+        {
+            if(id == ap.ID_Ap)
+            {
+                TrueHomeContext.updateApartment(ap);
+            }
+            return NoContent();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            TrueHomeContext.deleteApartment(id);
+            return NoContent();
+        }
+    }
+}
