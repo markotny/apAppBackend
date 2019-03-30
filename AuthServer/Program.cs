84ddc32c6dff3d224a8aee340ca92da677bf2c8e
Marcin Kotas <markotny97@gmail.com>
2019-03-30 11:22:44 +0100

    disabled HTTPS on Kestrel, configured docker for remote machine

--- AuthServer/Program.cs
+++ AuthServer/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -19,6 +20,10 @@ namespace AuthServer
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>                              // overwrites Kestrel addresses to only enable HTTP
+                {
+                    options.Listen(IPAddress.Any, 80);         // http:*:80
+                })
                 .UseStartup<Startup>();
     }
 }
