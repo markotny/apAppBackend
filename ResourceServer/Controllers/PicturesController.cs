31264385264cc0e243f8f76d2f6d6b9b3773a5d7
Marcin Kotas <markotny97@gmail.com>
2019-04-10 14:37:06 +0200

    Added Pictures controller

---
+++ ResourceServer/Controllers/PicturesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Logging;
+
+namespace ResourceServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PicturesController : ControllerBase
+    {
+        private readonly IContentTypeProvider _contentTypeProvider;
+        private readonly ILogger<PicturesController> _logger;
+
+        public PicturesController(
+            IContentTypeProvider contentTypeProvider,
+            ILogger<PicturesController> logger)
+        {
+            _contentTypeProvider = contentTypeProvider;
+            _logger = logger;
+        }
+
+        [HttpGet("{idAp}/{filename}")]
+        public async Task<IActionResult> Get(int idAp, string filename)
+        {
+            var path = "/data/pictures/" + idAp + "/" + filename;
+            try
+            {
+                _logger.LogDebug("Fetching picture from " + path);
+                var pic = await System.IO.File.ReadAllBytesAsync(path);
+                return File(pic, MapContentType(path));
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                _logger.LogError("Picture not found!");
+                return NotFound();
+            }
+        }
+
+
+        private string MapContentType(string path)
+        {
+            if (!_contentTypeProvider.TryGetContentType(path, out var contentType))
+                contentType = "application/octet-stream";
+            return contentType;
+        }
+    }
+}
--- ResourceServer/Startup.cs
+++ ResourceServer/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -42,6 +43,8 @@ namespace ResourceServer
         public void ConfigureServices(IServiceCollection services)
         {
             IdentityModelEventSource.ShowPII = true;    //for debugging
+
+            services.AddSingleton<IContentTypeProvider>(new FileExtensionContentTypeProvider());
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
