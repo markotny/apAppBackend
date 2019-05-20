6881f734eb99bf3f15324714249a9c1d9f7b41eb
Marcin Kotas <markotny97@gmail.com>
2019-05-20 18:54:44 +0200

    created rating table and trigger

--- postgresDB_scripts/ResSrvMigrate.sql
+++ postgresDB_scripts/ResSrvMigrate.sql
@@ -20,22 +20,40 @@ CREATE TABLE "user" (
 );
 
 CREATE TABLE "apartment" (
-	ID_Ap         	SERIAL,
-	Name           	varchar (100) NOT NULL,
-	City            varchar (100) NOT NULL,
-	Street          varchar (100) NOT NULL,
-	ApartmentNumber varchar (20)  NOT NULL,
-	ImgThumb        varchar (200),
-	ImgList         varchar (200)[],
-	Rate            numeric (2,1),
-	Lat             numeric (9,7) NOT NULL,
-	Long            numeric (10,7)NOT NULL,
-	Description 	text,
-	IDUser          text NOT NULL,
+	ID_Ap         		SERIAL,
+	Name           		varchar (100) NOT NULL,
+	City            	varchar (100) NOT NULL,
+	Street          	varchar (100) NOT NULL,
+	ApartmentNumber 	varchar (20)  NOT NULL,
+	ImgThumb        	varchar (200),
+	ImgList         	varchar (200)[],
+	OwnerRatingSum  	INTEGER DEFAULT 0,
+	LocationRatingSum  	INTEGER DEFAULT 0,
+	StandardRatingSum  	INTEGER DEFAULT 0,
+	PriceRatingSum     	INTEGER DEFAULT 0,
+	RatingsCount		INTEGER DEFAULT 0,
+	Lat             	numeric (9,7) NOT NULL,
+	Long            	numeric (10,7)NOT NULL,
+	Description 		text,
+	IDUser          	text NOT NULL,
 	CONSTRAINT pk_apartment PRIMARY KEY(ID_Ap),
 	CONSTRAINT fk_user FOREIGN KEY(IDUser) REFERENCES "user"(ID_User) ON DELETE RESTRICT
 );
 
+CREATE TABLE "rating" (
+	ID_Rating		SERIAL,
+	Owner			numeric (1) NOT NULL,
+	Location		numeric (1) NOT NULL,
+	Standard		numeric (1) NOT NULL,
+	Price			numeric (1) NOT NULL,
+	Description		text,
+	IDUser			text NOT NULL,
+	IDAp			INTEGER NOT NULL,
+	CONSTRAINT pk_rating PRIMARY KEY (ID_Rating),
+	CONSTRAINT fk_user FOREIGN KEY(IDUser) REFERENCES "user"(ID_User) ON DELETE RESTRICT,
+	CONSTRAINT fk_apartment FOREIGN KEY(IDAp) REFERENCES "apartment"(ID_Ap) ON DELETE RESTRICT
+);
+	
 CREATE TABLE "personaldata" (
 	ID_PData	SERIAL,
 	FirstName	varchar(50) NOT NULL,
@@ -49,3 +67,49 @@ CREATE TABLE "personaldata" (
 CREATE TABLE "dump" (
 	ID_PData	SERIAL
 );
+
+CREATE OR REPLACE FUNCTION update_ratings()
+	RETURNS TRIGGER AS $add_ratings$
+DECLARE
+   OwnerDiff	INTEGER := 0;
+   LocationDiff	INTEGER := 0;
+   StandardDiff	INTEGER := 0;
+   PriceDiff	INTEGER := 0;
+   CountDiff	INTEGER := 0;
+BEGIN
+	CASE TG_OP
+		WHEN 'INSERT' THEN
+			OwnerDiff = NEW.Owner;
+			LocationDiff = NEW.Location;
+			StandardDiff = NEW.Standard;
+			PriceDiff = NEW.Price;
+			CountDiff = 1;
+		WHEN 'UPDATE' THEN
+			OwnerDiff = NEW.Owner - OLD.Owner;
+			LocationDiff = NEW.Location - OLD.Location;
+			StandardDiff = NEW.Standard - OLD.Standard;
+			PriceDiff = NEW.Price - OLD.Price;
+		WHEN 'DELETE' THEN
+			OwnerDiff = - OLD.Owner;
+			LocationDiff = - OLD.Location;
+			StandardDiff = - OLD.Standard;
+			PriceDiff = - OLD.Price;
+			CountDiff = -1;
+	END CASE;
+	
+   UPDATE apartment
+		SET
+			OwnerRatingSum = OwnerRatingSum + OwnerDiff,
+			LocationRatingSum = LocationRatingSum + LocationDiff,
+			StandardRatingSum = StandardRatingSum + StandardDiff,
+			PriceRatingSum = PriceRatingSum + PriceDiff,
+			RatingsCount = RatingsCount + CountDiff
+		WHERE
+			ID_Ap = NEW.IDAp;
+   RETURN NEW;
+END
+$add_ratings$ LANGUAGE plpgsql;
+			
+CREATE TRIGGER rating_update
+    AFTER INSERT OR UPDATE OR DELETE ON rating
+    FOR EACH ROW EXECUTE PROCEDURE update_ratings();
--- postgresDB_scripts/UsersInsert.sql
+++ postgresDB_scripts/UsersInsert.sql
@@ -12,10 +12,23 @@ VALUES ('fddb44a3-43ae-44e2-b8a2-0962fa6be039','a@a.a','a@a.a','f',1);
 INSERT INTO "personaldata"(FirstName, LastName, BirthDate, IDUser)
 VALUES('Jan', 'Kowalski', '1990-06-01','fddb44a3-43ae-44e2-b8a2-0962fa6be039');
 
-INSERT INTO "apartment" (name,city,street,apartmentNumber,imgthumb,imglist,rate,lat,long,iduser) 
+INSERT INTO "apartment" (name,city,street,apartmentNumber,imgthumb,imglist,lat,long,iduser)
 VALUES 
-('luksus','Wrocław','Sienkiewicza','21/31','https://media-cdn.trulia-local.com/neighborhood-media-service-prod/il/chicago/south-loop/920-il_chi_south_loop_269607_177_256x256_cfill.jpg','{1.jpg,2.jpg}',3,'-39.98894','27.16119','fddb44a3-43ae-44e2-b8a2-0962fa6be039'),
-('mieszkanko','D�gelis','Ap #988-1740 Nunc Rd.','653-7584 Tortor, St.','https://media-cdn.trulia-local.com/neighborhood-media-service-prod/tx/austin/south-lamar/282-tx_aus_south_lamar_271570_2350_256x256_cfill.jpg','{1.jpg,2.jpg}',5,'19.10539','11.74472','fddb44a3-43ae-44e2-b8a2-0962fa6be039'),
-('domek','Le Puy-en-Velay','6353 Ut, Ave','5','http://www.gatewayapartments.com.hk/img/GatewayApartments.png','{1.jpg,2.jpg}',5,'25.23089','112.71901','fddb44a3-43ae-44e2-b8a2-0962fa6be039'),
-('zameczek na nadodrzu','Purranque','Ap #891-8040 Nec Rd.','2','https://media-cdn.trulia-local.com/neighborhood-media-service-prod/il/chicago/south-loop/920-il_chi_south_loop_269607_177_256x256_cfill.jpg','{1.jpg,2.jpg}',1,'-3.07401','-93.0161','fddb44a3-43ae-44e2-b8a2-0962fa6be039'),
-('kawalerka w piwnicy','Dutse','813-1033 Vitae Rd.','10A ','https://media-cdn.trulia-local.com/neighborhood-media-service-prod/tx/austin/south-lamar/282-tx_aus_south_lamar_271570_2350_256x256_cfill.jpg','{1.jpg,2.jpg}',2,'18.29239','-132.24146','fddb44a3-43ae-44e2-b8a2-0962fa6be039');
+('luksus','Wrocław','Sienkiewicza','21/31','https://media-cdn.trulia-local.com/neighborhood-media-service-prod/il/chicago/south-loop/920-il_chi_south_loop_269607_177_256x256_cfill.jpg','{1.jpg,2.jpg}','-39.98894','27.16119','fddb44a3-43ae-44e2-b8a2-0962fa6be039'),
+('mieszkanko','D�gelis','Ap #988-1740 Nunc Rd.','653-7584 Tortor, St.','https://media-cdn.trulia-local.com/neighborhood-media-service-prod/tx/austin/south-lamar/282-tx_aus_south_lamar_271570_2350_256x256_cfill.jpg','{1.jpg,2.jpg}','19.10539','11.74472','fddb44a3-43ae-44e2-b8a2-0962fa6be039'),
+('domek','Le Puy-en-Velay','6353 Ut, Ave','5','http://www.gatewayapartments.com.hk/img/GatewayApartments.png','{1.jpg,2.jpg}','25.23089','112.71901','fddb44a3-43ae-44e2-b8a2-0962fa6be039'),
+('zameczek na nadodrzu','Purranque','Ap #891-8040 Nec Rd.','2','https://media-cdn.trulia-local.com/neighborhood-media-service-prod/il/chicago/south-loop/920-il_chi_south_loop_269607_177_256x256_cfill.jpg','{1.jpg,2.jpg}','-3.07401','-93.0161','fddb44a3-43ae-44e2-b8a2-0962fa6be039'),
+('kawalerka w piwnicy','Dutse','813-1033 Vitae Rd.','10A ','https://media-cdn.trulia-local.com/neighborhood-media-service-prod/tx/austin/south-lamar/282-tx_aus_south_lamar_271570_2350_256x256_cfill.jpg','{1.jpg,2.jpg}','18.29239','-132.24146','fddb44a3-43ae-44e2-b8a2-0962fa6be039');
+
+INSERT INTO "rating" (Owner,Location,Standard,Price,Description,IDUser,IDAp)
+VALUES
+(4,5,1,2,'takie se','fddb44a3-43ae-44e2-b8a2-0962fa6be039',1),
+(5,1,5,4,'takie se','fddb44a3-43ae-44e2-b8a2-0962fa6be039',2),
+(3,3,3,3,'takie se','fddb44a3-43ae-44e2-b8a2-0962fa6be039',3),
+(2,5,2,5,'takie se','fddb44a3-43ae-44e2-b8a2-0962fa6be039',4),
+(1,4,3,1,'takie se','fddb44a3-43ae-44e2-b8a2-0962fa6be039',5),
+(4,5,4,2,'takie se','fddb44a3-43ae-44e2-b8a2-0962fa6be039',1),
+(3,1,3,1,'takie se','fddb44a3-43ae-44e2-b8a2-0962fa6be039',2),
+(3,2,3,1,'takie se','fddb44a3-43ae-44e2-b8a2-0962fa6be039',3),
+(4,5,1,5,'takie se','fddb44a3-43ae-44e2-b8a2-0962fa6be039',4),
+(5,5,1,4,'takie se','fddb44a3-43ae-44e2-b8a2-0962fa6be039',5);
